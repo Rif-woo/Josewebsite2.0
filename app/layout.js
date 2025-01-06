4cd835275a15c4f2f8cc5d0f78d469253c8da465
cnzale <azermax123zale@gmail.com>
2025-01-06 19:47:31 +0000

    google analytics implemented update

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGZLPQ8C');`,
+          }}
+        />
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=GTM-KGZLPQ8C"
         />
@@ -37,6 +46,14 @@ export default function RootLayout({ children }) {
         {/* Script Google Analytics */}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGZLPQ8C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <Analytics />
       </body>
